c4558a61dc64bfecef1b28db2f0b9997152f2d48
José Bonilla <62049370+PepBonill@users.noreply.github.com>
2021-12-12 20:05:57 -0600

    Delete dashboard directory

--- dashboard/appbienvenida/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppbienvenidaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appbienvenida'
--- dashboard/dashboard/dash_apps/Patrones-main/asc desc lat/prueba.py
+++
@@ -1,7 +0,0 @@
-import os
-from os import listdir
-from os.path import isfile, join
-
-print(os.path)
-
-#onlyfiles = [f for f in listdir(/) if isfile(join(os.path(), f))]
